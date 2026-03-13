30007e4f593d090eb0f64bb8ecf0a89d3ca42b99
yuzhe <53376445+bkmashiro@users.noreply.github.com>
2026-03-13 03:24:19 +0000

    chore: bump version to 1.2.10

--- src/index.ts
+++ src/index.ts
@@ -5,7 +5,7 @@
  */
 
 // eslint-disable-next-line @typescript-eslint/no-var-requires
-export const version = '1.2.9'
+export const version = '1.2.10'
 
 import { Lexer } from './lexer'
 import { Parser } from './parser'
